f4339726c53a0674cff02f67a107d8657bc8b318
Alina Turlaeva <alin.alin.alin2000@yandex.ru>
2026-04-08 11:18:35 +0300

    Initial Commit

---
+++ MoexCalculator/MoexCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoexCalculatorApp.swift
+//  MoexCalculator
+//
+//  Created by Alina on 08/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct MoexCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
